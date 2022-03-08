00390f027450bac087f8ffabe7f425ca89a30a98
Gabriel Lemes <gabriellemesroutlook.com>
2022-03-08 17:49:10 +0000

    service layer - begin user implementation

---
+++ DDDModel/DDDModel.Service/Services/UserService.cs
@@ -0,0 +1,49 @@
+﻿using DDDModel.Domain.Entities;
+using DDDModel.Domain.Interfaces;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDDModel.Service.Services
+{
+    public class UserService<User> : IBaseService<User> where User : BaseEntity
+    {
+
+        private readonly IBaseRepository<User> _baseRepository;
+
+        public UserService(IBaseRepository<User> baseRepository)
+        {
+            _baseRepository = baseRepository;
+        }
+
+
+        public User Add<UserValidator>(User obj) where UserValidator : AbstractValidator<User>
+        {
+            //_baseRepository.Insert(obj);
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<User> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Update<UserValidator>(User obj) where UserValidator : AbstractValidator<User>
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
