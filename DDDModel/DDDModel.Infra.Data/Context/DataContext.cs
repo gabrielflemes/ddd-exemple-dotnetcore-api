89954914005ded22070b25158b295ce7ccae9e7f
Gabriel Lemes <gabriellemesroutlook.com>
2022-03-08 17:32:44 +0000

    Repository Implementation

---
+++ DDDModel/DDDModel.Infra.Data/Context/DataContext.cs
@@ -0,0 +1,28 @@
+﻿using DDDModel.Domain.Entities;
+using DDDModel.Infra.Data.Mapping;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDDModel.Infra.Data.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>(new UserMap().Configure);
+        }
+    }
+}
